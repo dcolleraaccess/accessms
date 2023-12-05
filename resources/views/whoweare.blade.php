3a06c0b4334fce590f31c3e4c57f97ef34d071a3
dandrecollera <dandrecollera@gmail.com>
2023-12-05 23:50:55 +0800

    about mobile

--- resources/views/whoweare.blade.php
+++ resources/views/whoweare.blade.php
@@ -40,16 +40,16 @@
 
 
 
-        <div class="container-xl py-5">
+        <div class="container-xl py-2">
             <div class="row d-flex align-items-center">
 
                 <iframe src="https://www.youtube.com/embed/_twgLLdpVaY"
                     title="Get To Know the Owner and CEO of Access Healthcare – Danielle Doyle" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen class="col-6" height="475"></iframe>
+                    allowfullscreen class="col-lg-6 rounded-5 mb-4 mb-lg-0" height="475"></iframe>
 
 
-                <div class="col-6">
+                <div class="col-lg-6">
                     <center>
 
                         <h1>Be Updated</h1>
@@ -82,8 +82,8 @@
     <div class="container-xl py-5 my-3">
         <h1 class="text-center">Living Our Commitments</h1>
         <p class="text-center">At Access Healthcare, Success Begins with Our Core Values</p>
-        <div class="row my-3 py-4">
-            <div class="col-4 d-flex flex-column align-items-center">
+        <div class="row my-3 py-0 py-lg-4">
+            <div class="col-lg-4 mb-4 mb-lg-0 d-flex flex-column align-items-center">
                 <i class="fa-solid fa-handshake mb-3" style="font-size:50px"></i>
                 <h4>
                     Partnership
@@ -92,7 +92,7 @@
                     Fostering true collaboration, defining success beyond employer-employee norms with shared goals.
                 </center>
             </div>
-            <div class="col-4 d-flex flex-column align-items-center">
+            <div class="col-lg-4 mb-4 mb-lg-0 d-flex flex-column align-items-center">
                 <i class="fa-solid fa-heart mb-3" style="font-size:50px"></i>
                 <h4>
                     Compassion
@@ -100,7 +100,7 @@
                 <center>
                     Mission-driven care, guiding decisions with genuine concern for clients and nurses.</center>
             </div>
-            <div class="col-4 d-flex flex-column align-items-center">
+            <div class="col-lg-4 mb-4 mb-lg-0 d-flex flex-column align-items-center">
                 <i class="fa-solid fa-shield-alt mb-3" style="font-size:50px"></i>
                 <h4>
                     Integrity
@@ -109,8 +109,8 @@
                     Non-negotiable commitment to trust, founded on transparent and ethical practices.</center>
             </div>
         </div>
-        <div class="row my-3 py-4">
-            <div class="col-6 d-flex flex-column align-items-center">
+        <div class="row my-0 my-lg-3 py-0 py-lg-4">
+            <div class="col-lg-6 mb-4 mb-lg-0 d-flex flex-column align-items-center">
                 <i class="fa-solid fa-lightbulb mb-3" style="font-size:50px"></i>
                 <h4>
                     Innovation
@@ -118,7 +118,7 @@
                 <center>
                     Embracing evolution, staying ahead with forward-thinking strategies in healthcare staffing.</center>
             </div>
-            <div class="col-6 d-flex flex-column align-items-center">
+            <div class="col-lg-6 d-flex flex-column align-items-center">
                 <i class="fa-solid fa-rocket mb-3" style="font-size:50px"></i>
                 <h4>
                     Empowerment
@@ -131,12 +131,12 @@
 </section>
 
 <section>
-    <div class="container-xl py-3 my-5">
+    <div class="container-xl py-3 my-3">
 
         <div class="row d-flex align-items-center py-4 mb-4">
-            <div class="col-6 ">
-                <h1 class="text-end">Our Expertise</h1>
-                <p class="text-end">
+            <div class="col-lg-6">
+                <h1 class="text-center text-lg-end">Our Expertise</h1>
+                <p class="text-lg-end text-center">
                     Access Healthcare stands out with its fully developed divisions, including locum, travel
                     nursing/allied, per diem, and permanent placement. Our extensive reach spans 48 states, allowing us
                     to serve clients nationwide. The triple Joint Commission Certification underscores our commitment to
@@ -144,19 +144,20 @@
                     Agency by SIA in 2023, showcasing our dedication to continual growth and innovation in the industry.
                 </p>
             </div>
-            <div class="col-6">
+            <div class="col-lg-6">
                 <img src="{{asset('img/Locum Tenens.jpg')}}" class="d-block w-100 rounded-5 contentimg" alt="...">
             </div>
         </div>
 
 
         <div class="row d-flex align-items-center py-4">
-            <div class="col-6">
+            <div class="col-lg-6 order-2 order-lg-1">
                 <img src="{{asset('img/Permanent.jpg')}}" class="d-block w-100 rounded-5 contentimg" alt="...">
             </div>
-            <div class="col-6 ">
-                <h1>Global Reach and Local Impact</h1>
-                <p>While based in New Jersey, our impact extends far beyond state borders. As a Minority Designated
+            <div class="col-lg-6 order-1 order-lg-2">
+                <h1 class="text-center text-lg-start">Global Reach and Local Impact</h1>
+                <p class="text-center text-lg-start">While based in New Jersey, our impact extends far beyond state
+                    borders. As a Minority Designated
                     Firm, we bring diversity and inclusivity to our services, contributing to the improvement of
                     healthcare access and delivery nationwide. Our offshore team in the Philippines further enhances our
                     operational efficiency, ensuring seamless back-office support. Through a combination of global reach
